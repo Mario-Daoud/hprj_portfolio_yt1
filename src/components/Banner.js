0fac913740ad87661d7ea69325bb6c657258b525
Mario Daoud <r0895582@student.vives.be>
2023-03-03 22:49:07 +0100

    added newsletter & added footer & added contact

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,26 +20,26 @@ export const Banner = () => {
         return () => { clearInterval(ticker) };
     }, [text])
 
-const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+        setText(updatedText);
 
-    if(isDeleting) {
-        setDelta(prevDelta => prevDelta/2)
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
     }
-
-    if(!isDeleting && updatedText === fullText) {
-        setIsDeleting(true);
-        setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-        setIsDeleting(false);
-        setLoopNum(loopNum + 1);
-        setDelta(500);
-    }
-}
 
     return (
         <section className="banner" id="home">
@@ -47,7 +47,7 @@ const tick = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Mario Daoud `}<br/><span className="wrap">{text}</span></h1>
+                        <h1>{`Hi I'm Mario Daoud `}<br /><span className="wrap">{text}</span></h1>
                         <p>I'm a second year software developing student at VIVES Hogeschool, Campus Kortrijk.</p>
                         <button onClick={() => console.log('connect')}>Let's connect! <ArrowRightCircle size={25} /></button>
                     </Col>
